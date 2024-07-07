b845961101689abef6d35e1b1e34dd5a1d611b0e
massi.gerardi <massi@ambulando.net>
2024-07-07 10:37:49 +0200

    feat: draft for getting transactions info

--- extract/transactions.py
+++ extract/transactions.py
@@ -1,5 +1,43 @@
+from dataclasses import dataclass
+
+from pandas import DataFrame
+
 from extract import Contract
+
+@dataclass
+class Transfer:
+    from_address: str
+    to_address: str
+    value: int
 
 
 def extract_data(contract: Contract, data):
-    pass
+    transactions = data["data"]["items"]
+    transfers = get_transfers(transactions, contract.address)
+    df = DataFrame(transfers)
+    # TODO add transfer stats to contract
+
+def get_transfers(txns, address):
+    transfers = []
+    for tx in txns:
+        logs = tx["log_events"]
+        for log in logs:
+            if "decoded" in log is None:
+                continue
+            decoded = log["decoded"]
+            if decoded["name"] != "Transfer":
+                continue
+            value = None
+            value_string = get_param(decoded["params"], "value")
+            if value_string is not None:
+                value = int(value_string)
+            transfer = Transfer(
+                from_address=get_param(decoded["params"], "from"),
+                to_address=get_param(decoded["params"], "to"),
+                value=value
+            )
+            transfers = [*transfers, transfer]
+        return transfers
+
+def get_param(params, name):
+    return next((x for x in params if x["name"] == name), None)
